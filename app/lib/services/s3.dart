e5de29fee253b0ec24fbae41cc60684493fbe493
tuimac <tuimac.devadm01@gmail.com>
2023-08-06 05:08:42 +0000

    save

--- app/lib/services/s3.dart
+++ app/lib/services/s3.dart
@@ -1,20 +1,22 @@
 import 'package:minio_new/minio.dart';
 import 'dart:typed_data';
+import 'dart:developer' as developer;
 
 class S3Service {
   static Future<String> getPasswordFile() async {
     final minio = Minio(
-      endPoint: 's3-ap-northeast-3.amazonaws.com',
+      endPoint: 's3.ap-northeast-3.amazonaws.com',
       region: 'ap-northeast-3',
-      accessKey: '',
-      secretKey: '',
       useSSL: true,
     );
+    developer.log(minio as String);
     final stream = await minio.getObject('tuimac-redmine', 'password.json');
     List<int> memory = [];
     await for (var value in stream) {
       memory.addAll(value);
     }
+    developer.log(String.fromCharCodes(Uint8List.fromList(memory)));
+    // ignore: unnecessary_cast
     return String.fromCharCodes(Uint8List.fromList(memory));
   }
 }
--- app/lib/widgets/list_widgets.dart
+++ app/lib/widgets/list_widgets.dart
@@ -9,12 +9,28 @@ class ListItems extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: const Text('Password Manager')),
         body: Center(
-          child: FutureBuilder<String>(
-              future: S3Service.getPasswordFile(),
-              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                <Widget>[const Text('fail')]
-              }),
-        ));
+            child: FutureBuilder<String?>(
+                future: S3Service.getPasswordFile(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    final error = snapshot.error;
+                    return Text(
+                      'Errro: $error',
+                      style: const TextStyle(fontSize: 20, color: Colors.white),
+                    );
+                  } else if (snapshot.hasData) {
+                    String result = snapshot.data!;
+                    return Text(
+                      result,
+                      style: const TextStyle(fontSize: 20, color: Colors.white),
+                    );
+                  } else {
+                    return const Text(
+                      "しばらくお待ち下さい",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    );
+                  }
+                })));
   }
 }
 
