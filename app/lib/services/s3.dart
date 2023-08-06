7746c8efe495972bef3184411568b60bd74ec87c
tuimac <tuimac.devadm01@gmail.com>
2023-08-06 03:33:02 +0000

    save

--- app/lib/services/s3.dart
+++ app/lib/services/s3.dart
@@ -1,5 +1,20 @@
-import 'package:flutter/material.dart';
 import 'package:minio_new/minio.dart';
 import 'dart:typed_data';
 
-class S3Service {}
+class S3Service {
+  static Future<String> getPasswordFile() async {
+    final minio = Minio(
+      endPoint: 's3-ap-northeast-3.amazonaws.com',
+      region: 'ap-northeast-3',
+      accessKey: '',
+      secretKey: '',
+      useSSL: true,
+    );
+    final stream = await minio.getObject('tuimac-redmine', 'password.json');
+    List<int> memory = [];
+    await for (var value in stream) {
+      memory.addAll(value);
+    }
+    return String.fromCharCodes(Uint8List.fromList(memory));
+  }
+}
--- app/lib/widgets/list_widgets.dart
+++ app/lib/widgets/list_widgets.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../services/s3.dart';
 
 class ListItems extends StatelessWidget {
   const ListItems({super.key});
@@ -6,11 +7,15 @@ class ListItems extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Password Manager')),
-      body: const Center(
-          child: Column(
-        children: <Widget>[Text('test', style: TextStyle(color: Colors.white))],
-      )),
-    );
+        appBar: AppBar(title: const Text('Password Manager')),
+        body: Center(
+          child: FutureBuilder<String>(
+              future: S3Service.getPasswordFile(),
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                <Widget>[const Text('fail')]
+              }),
+        ));
   }
 }
+
+class $ {}
