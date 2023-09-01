10f83955bebc864807ca316a46d5e6281b9281da
tuimac <tuimac.devadm01@gmail.com>
2023-09-01 21:04:51 +0900

    install icon launcher

--- src/lib/services/fileio.dart
+++ src/lib/services/fileio.dart
@@ -1,35 +1,64 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:developer';
 import 'package:intl/intl.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:src/utils/cipher.dart';
+import 'package:src/config/config.dart';
+import 'dart:developer';
 
 class FileIO {
   static Future<Map<String, dynamic>> get getData async {
     try {
       final baseDirInfo = await getApplicationDocumentsDirectory();
-      final pwdPath = '${baseDirInfo.path}/latest.pwdm';
-      log(pwdPath);
-      return jsonDecode(
-          Cipher.decryptString(await File(pwdPath).readAsString()));
-    } catch (e) {
-      return {
-        'passwords': {},
-        'setting': {'auto_backup': false}
-      };
+      return jsonDecode(Cipher.decryptString(await File(
+              '${baseDirInfo.path}${Config.dataDir}/${Config.latestData}')
+          .readAsString()));
+    } on PathNotFoundException {
+      initData();
+      return Config.dataTemplate;
     }
   }
 
-  static void saveData(data) async {
+  static Future saveData(data) async {
     final baseDirInfo = await getApplicationDocumentsDirectory();
-    final pwdPath = '${baseDirInfo.path}/latest.pwdm';
-    if (data['setting']['auto_backup']) {
-      final newPath =
-          '${baseDirInfo.path}/${DateFormat('yyyy-MM-dd-H-m-s').format(DateTime.now())}.pwdm';
-      await File(pwdPath).copy(newPath);
+    final pwdPath = '${baseDirInfo.path}${Config.dataDir}/${Config.latestData}';
+    try {
+      if (data['setting']['auto_backup']) {
+        final newPath =
+            '${baseDirInfo.path}${Config.dataDir}/${DateFormat('yyyy-MM-dd-H-m-s').format(DateTime.now())}${Config.dataExtension}';
+        await File(pwdPath).copy(newPath);
+      }
+      await File(pwdPath).writeAsString(Cipher.encryptString(jsonEncode(data)),
+          mode: FileMode.writeOnly);
+    } catch (e) {
+      rethrow;
     }
-    await File(pwdPath).writeAsString(Cipher.encryptString(jsonEncode(data)),
-        mode: FileMode.writeOnly);
+  }
+
+  static Future initData() async {
+    final baseDirInfo = await getApplicationDocumentsDirectory();
+    try {
+      List<FileSystemEntity> itemsInfo =
+          await Directory('${baseDirInfo.path}${Config.dataDir}')
+              .list()
+              .toList();
+      if (itemsInfo.isEmpty) {
+        await saveData(Config.dataTemplate);
+      } else {
+        try {
+          await File(
+                  '${baseDirInfo.path}${Config.dataDir}/${Config.latestData}')
+              .readAsString();
+        } on PathNotFoundException {
+          saveData(Config.dataTemplate);
+        }
+      }
+    } on PathNotFoundException {
+      await Directory('${baseDirInfo.path}${Config.dataDir}')
+          .create(recursive: true);
+      saveData(Config.dataTemplate);
+    } catch (e) {
+      rethrow;
+    }
   }
 }
